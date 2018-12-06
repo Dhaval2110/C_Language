279cfd64edb1fec8fe8b814ef2350eb0c0264a57
Dhaval <32478060+Dhaval2110@users.noreply.github.com>
2018-12-06 22:59:39 +0530

    FP Ex

--- function_pointer_easy.c
+++ function_pointer_easy.c
@@ -1,6 +1,7 @@
+#Ex 1
 #include<stdio.h>
 #include<stdlib.h>
-void fun()
+void fun()                        //function definition 
 {
 	printf("Hello\n");
 }
@@ -10,10 +11,26 @@ void fun1(int b)
 }
 main()
 {
-	void (*func[2])(int);
-	func[0]=&fun;
+	void (*func[2])(int);          //same syntax should be define as function which you have to call as a fp
+	func[0]=&fun;                  //mapping a function to pointer
 	func[1]=&fun1;
-	(*func[0])(10);
+	(*func[0])(10);                //most imp : call
 	(*func[1])(30);
 	
+}
+
+#Ex 2
+
+int clear(int n,int p)
+{
+	n=n&~(1<<p);
+	return n;
+}
+main()
+{
+	int r;
+	int (*fptr)(int,int);
+	fptr=&clear;
+	r=clear(15,2);
+	printf("%d",r);
 }
