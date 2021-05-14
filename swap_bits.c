52da9da1cdb4812c7bda1061e9a18104a86fa02d
Dhaval <32478060+Dhaval2110@users.noreply.github.com>
2021-05-14 17:22:01 +0530

    swap bits

--- swap_bits.c
+++ swap_bits.c
@@ -16,3 +16,19 @@ int main()
     printf("Result = %d", swapBits(28, 0, 3));              //28 is num, 0 is postion 1 and 3 is position2, 0 and 3 to be swapped;
     return 0;
 }
+
+
+/// SWAP NIBBLES
+#include <stdio.h>
+ 
+unsigned char swapNibbles(unsigned char x)
+{
+    return ( (x & 0x0F)<<4 | (x & 0xF0)>>4 );
+}
+ 
+int main()
+{
+    unsigned char x = 100;
+    printf("%u", swapNibbles(x));
+    return 0;
+}
